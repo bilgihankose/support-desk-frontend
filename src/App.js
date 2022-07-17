aa235e6331c55878df26f19105975aa3f8a06002
Bilgihan Kose <bilgihankose@gmail.com>
2022-07-17 19:41:32 +0300

    Create initial pages and header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
